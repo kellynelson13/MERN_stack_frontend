ba9f2a55dc43140d5fd981e586a33092bc075bdd
Kelly Nelson <kelnel86@gmail.com>
2022-09-28 08:45:25 -0500

    adds toml file and scripts to prevent netlify error

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ const Main = (props) => {
   
   const [ people, setPeople ] =useState(null)
   
-  const URL = "https://people-app-ga-titans.herokuapp.com/people"
+  const URL = "https://people-app-ga-titans.herokuapp.com/people/"
    
   const getPeople = async () => {
     const response = await fetch(URL)
@@ -29,7 +29,27 @@ const Main = (props) => {
     getPeople()
   }
 
-//   useEffect(() => getPeople(), [])
+  const updatePeople = async (person, id) => {
+    // make put request to create people
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    // update list of people
+    getPeople();
+  }
+
+  const deletePeople = async (id) => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    })
+    // update list of people
+    getPeople();
+  }
 
     useEffect(() => {
         getPeople();
@@ -48,6 +68,9 @@ const Main = (props) => {
                 path="/people/:id"
                 render={(rp) => (
                     <Show 
+                        people={people}
+                        updatePeople={updatePeople}
+                        deletePeople={deletePeople}
                         {...rp}
                     />
                 )}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Show = (props) => {
+    const id = props.match.params.id;
+    const people = props.people;
+    const person = people.find(p => p._id === id);
+
+    //state for form
+    const [editForm, setEditForm] = useState(person);
+
+    // handleChange function for form
+    const handleChange = (e) => {
+        setEditForm({...editForm, [e.target.name]: e.target.value})
+    }
+
+    //handleSubmit for form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.updatePeople(editForm, person._id);
+        // redirect people back to index
+        props.history.push("/");
+    }
+
+    const removePerson = () => {
+        props.deletePeople(person._id);
+        props.history.push("/");
+    }
+
   return (
-    <div>Show</div>
+    <div className="person">
+        <h1>{person.name}</h1>
+        <h2>{person.title}</h2>
+        <img src={person.image} alt={person.name}/>
+        <button id="delete" onClick={removePerson}>
+        DELETE
+      </button>
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Person" />
+      </form>
+    </div>
   )
 }
 
